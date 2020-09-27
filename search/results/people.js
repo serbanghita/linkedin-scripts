7fde47d72f511b2d0b554235e201c89eaa9ab7df
Serban Ghita <serbanghita@gmail.com>
2020-09-27 21:11:14 +0300

    Added explanation about the filters.

--- search/results/people.js
+++ search/results/people.js
@@ -1,6 +1,7 @@
 /**
  * 1. Go to https://www.linkedin.com/in/{contact}
  * 2. On the profile page navigate to "400 connections"
+ *      - apply any filters you want to the page.
  * 3. Press F12 (open Chrome Developer Tools).
  * 4. Copy and apply script in the console.
  * 5. Wait for the navigation to finish.
